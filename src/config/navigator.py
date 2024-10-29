866d46d5720833016ff82fc12995fa94f2b4513e
Ilia Tambovtsev <ilia.tambovtsev@raftds.com>
2024-10-29 16:56:43 +0300

    style: untab code

--- src/config/navigator.py
+++ src/config/navigator.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from dataclasses import dataclass
-from typing import Union
+from typing import List, Optional
 
 @dataclass
 class Navigator:
@@ -20,11 +20,44 @@ class Navigator:
             path.mkdir(parents=True, exist_ok=True)
 
     def get_interim_path(self, presentation_name: str) -> Path:
-            """Get interim path for specific presentation"""
-            path = self.interim / presentation_name
-            path.mkdir(parents=True, exist_ok=True)
-            return path
+        """Get interim path for specific presentation"""
+        path = self.interim / presentation_name
+        path.mkdir(parents=True, exist_ok=True)
+        return path
 
     def get_processed_path(self, filename: str) -> Path:
         """Get path for processed results"""
         return self.processed / filename
+
+    def find_file_by_substr(
+            self,
+            substr: str,
+            extension: Optional[str] = None,
+            base_dir: Optional[Path] = None,
+        ) -> List[Path]:
+        """
+        Find file by substring.
+
+        Args:
+            substr: substring to search for
+            extension: [".png", ".pdf"] file extension to filter by (optional)
+
+        Example:
+            -$ find_by_substr("Kolm")
+            -> <path to presentation about Kolmogorov Networks>
+        """
+        if extension is None:
+            extension = ""
+
+        search_pattern = fr"*{substr}*"
+
+        if base_dir is None:
+           base_dir = self.data
+
+        results = list(base_dir.rglob(search_pattern))
+        if extension is not None:
+            results = [path for path in results if path.name.endswith(extension)]
+        if len(results) > 1:
+            print(f"Found {len(results)} matches for {substr}")
+
+        return results
