b49f5ea427717c8e8c4ac8a821dca8e918802fc2
Ilia Tambovtsev <ilia.tambovtsev@raftds.com>
2024-11-07 15:40:49 +0300

    feat: update imports

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,2 +1,6 @@
 from src.config.multiline_wrapper import MultilineWrapper
 from src.config.navigator import Navigator
+from src.config.model_setup import ModelConfig
+
+from src.config.config import Config
+
